03a22709b81331b1f6efd3b9c658c14501b08535
shikai <shikai_1989621@126.com>
2015-12-30 21:42:16 +0800

    Initial commit
     Changes to be committed:
    	new file:   test_time.cpp

---
+++ test_time.cpp
@@ -0,0 +1,9 @@
+#include <windows.h> 
+#include <stdio.h> 
+int main() 
+{ 
+SYSTEMTIME sys; 
+GetLocalTime(&sys); 
+printf( "%4d/%02d/%02d %02d:%02d:%02d\n",sys.wYear,sys.wMonth,sys.wDay,sys.wHour,sys.wMinute, sys.wSecond); 
+return 0;
+}
